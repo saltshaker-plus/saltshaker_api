56f150989b3d0a2f52b3becf4e0bb063c3bcda77
yueyongyue <yongpeng1@staff.sina.com.cn>
2018-03-15 16:36:02 +0800

    Deleted Flask HTTPAuth

--- resources/log.py
+++ resources/log.py
@@ -1,49 +1,19 @@
 # -*- coding:utf-8 -*-
+from flask import g
 from flask_restful import Resource, reqparse
-from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth, MultiAuth
 from common.log import Logger
 from common.db import DB
-from common.utility import verify_password, verify_token
-from flask import make_response, jsonify, g
-
+from common.sso import login_required
 
 logger = Logger()
-basic_auth = HTTPBasicAuth()
-token_auth = HTTPTokenAuth(scheme='Bearer')
-multi_auth = MultiAuth(basic_auth, token_auth)
-
 
 parser = reqparse.RequestParser()
 parser.add_argument("product_id", type=str, required=True, trim=True)
 
 
-# 错误提示重新定义
-@basic_auth.error_handler
-def unauthorized():
-    return make_response(jsonify({"status": False, "message": "Unauthorized access"}), 401)
-
-
-@token_auth.error_handler
-def unauthorized():
-    return make_response(jsonify({"status": False, "message": "Token error or expiration"}), 401)
-
-
-# 验证密码
-@basic_auth.verify_password
-def auth_password(username, password):
-    return verify_password(username, password)
-
-
-# 验证Token
-@token_auth.verify_token
-def auth_token(token):
-    return verify_token(token)
-
-
 class LogList(Resource):
-    @multi_auth.login_required
+    @login_required
     def get(self):
-        print(g.user)
         args = parser.parse_args()
         db = DB()
         status, result = db.select_by_id("audit_log", args["product_id"])
@@ -55,7 +25,6 @@ class LogList(Resource):
         else:
             return {"status": False, "message": result}, 200
         return {"audit_logs": {"audit_log": log_list}}
-
 
 
 
