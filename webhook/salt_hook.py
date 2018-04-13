715f448d6b18ea5e370330872f49c73aaf7b75fd
yueyongyue <yongpeng1@staff.sina.com.cn>
2018-04-13 12:24:40 +0800

    Added hook get to post

--- webhook/salt_hook.py
+++ webhook/salt_hook.py
@@ -9,12 +9,12 @@ logger = Logger()
 
 parser = reqparse.RequestParser()
 parser.add_argument("product_id", type=str, required=True, trim=True)
-parser.add_argument("tag", type=str, required=True, trim=True)
+parser.add_argument("tag", type=str, default="gitfs/update", trim=True)
 
 
 class Hook(Resource):
     @access_required(role_dict["common_user"])
-    def get(self):
+    def post(self):
         args = parser.parse_args()
         salt_api = salt_api_for_product(args["product_id"])
         if isinstance(salt_api, dict):
