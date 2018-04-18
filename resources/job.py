aaa28ebcfb0771224c12d4baba14fc6d6d316773
yueyongyue <yongpeng1@staff.sina.com.cn>
2018-04-18 21:19:17 +0800

    Changed job manage delete to post

--- resources/job.py
+++ resources/job.py
@@ -14,6 +14,7 @@ parser = reqparse.RequestParser()
 parser.add_argument("product_id", type=str, required=True, trim=True)
 parser.add_argument("action", type=str, trim=True)
 parser.add_argument("jid", type=str, trim=True)
+parser.add_argument("minion_id", type=str, trim=True, action="append")
 
 
 class Job(Resource):
@@ -65,15 +66,25 @@ class JobManager(Resource):
     def get(self):
         args = parser.parse_args()
         salt_api = salt_api_for_product(args["product_id"])
+        job_active_list = []
         if isinstance(salt_api, dict):
             return salt_api, 500
         else:
             result = salt_api.runner("jobs.active")
-            return result, 200
+            if result:
+                if result.get("status") is False:
+                    return result, 500
+                for jid, info in result.items():
+                            # 不能直接把info放到append中
+                            info.update({"Jid": jid})
+                            job_active_list.append(info)
+            return {"data": job_active_list, "status": True, "message": ""}, 200
 
     @access_required(role_dict["common_user"])
-    def delete(self):
+    def post(self):
         args = parser.parse_args()
+        salt_api = salt_api_for_product(args["product_id"])
+        user = g.user_info["username"]
         db = DB()
         status, result = db.select_by_id("product", args["product_id"])
         db.close_mysql()
@@ -84,18 +95,19 @@ class JobManager(Resource):
                 return {"status": False, "message": "%s does not exist" % args["product_id"]}
         else:
             return {"status": False, "message": result}
-        salt_api = salt_api_for_product(args["product_id"])
-        user = g.user_info["username"]
         if isinstance(salt_api, dict):
             return salt_api, 500
         else:
-            if args["action"] == "kill" and args["jid"]:
-                kill = "salt %s saltutil.kill_job %s" % (product.get("salt_master_id"), args["jid"])
-                try:
-                    result = salt_api.shell_remote_execution(product.get("salt_master_id"), kill)
-                    audit_log(user, args["jid"], args["product_id"], "job id", "kill")
-                    return {"status": True, "message": result}, 200
-                except Exception as e:
-                    return {"status": False, "message": str(e)}, 500
+            if args["action"] == "kill" and args["jid"] and args["minion_id"]:
+                for minion in args["minion_id"]:
+                    kill = "salt %s saltutil.kill_job %s" % (minion, args["jid"])
+                    try:
+                        result = salt_api.shell_remote_execution(product.get("salt_master_id"), kill)
+                        audit_log(user, args["jid"], args["product_id"], "job id", "kill")
+                        logger.info("kill %s %s return: %s" % (minion, args["jid"], result))
+                    except Exception as e:
+                        logger.info("kill %s %s error: %s" % (minion, args["jid"], e))
+                return {"status": True, "message": ""}, 200
             else:
-                return {"status": False, "message": "The specified jid parameter does not exist"}, 400
+                return {"status": False, "message": "The specified jid or action or minion_id "
+                                                    "parameter does not exist"}, 400
