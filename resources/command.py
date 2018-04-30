e3591833bce989697d96d3fba416a6c9d385fb8a
yueyongyue <yongpeng1@staff.sina.com.cn>
2018-04-30 08:40:14 +0800

    Changed username

--- resources/command.py
+++ resources/command.py
@@ -19,18 +19,27 @@ class ShellList(Resource):
         status, result = db.select("cmd_history", "where data -> '$.product_id'='%s' order by data -> '$.time' desc"
                                    % args["product_id"])
         history_list = []
+        user_list = []
+        user_status, user_result = db.select("user", "")
+        if user_status is True and user_result:
+            for i in user_result:
+                try:
+                    user_list.append(eval(i[0]))
+                except Exception as e:
+                    db.close_mysql()
+                    return {"status": False, "message": str(e)}, 500
+        db.close_mysql()
+        print(user_list)
         if status is True:
             if result:
                 for i in result:
                     history_dict = eval(i[0].replace('true', '"true"').replace('false', '"false"').replace('null', '""'))
-                    user_status, user_result = db.select_by_id("user", history_dict["user_id"])
-                    if user_status is True and user_result:
-                        history_dict["username"] = eval(user_result[0][0])["username"]
+                    for user in user_list:
+                        if history_dict["user_id"] == user["id"]:
+                            history_dict["username"] = user["username"]
                     history_list.append(history_dict)
         else:
-            db.close_mysql()
             return {"status": False, "message": result}, 500
-        db.close_mysql()
         return {"data": history_list, "status": True, "message": ""}, 200
 
 
