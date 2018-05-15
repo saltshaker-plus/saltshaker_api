0e9fe49b7ce3178df6933fb9073b284be7c5d0c7
yueyongyue <yongpeng1@staff.sina.com.cn>
2018-05-15 20:46:34 +0800

    Added send mail function

---
+++ common/send_mail.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+from common.log import loggers
+import configparser
+import os
+from email.header import Header
+from email.mime.text import MIMEText
+import smtplib
+
+logger = loggers()
+
+config = configparser.ConfigParser()
+conf_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+config.read(conf_path + "/saltshaker.conf")
+from_addr = config.get("Mail", "FROM_ADDR")
+password = config.get("Mail", "PASSWORD")
+smtp_server = config.get("Mail", "SMTP_SERVER")
+
+
+def send_mail(to_addr, sub, content):
+    msg = MIMEText(content, 'plain', 'utf-8')
+    msg['From'] = "Saltshaker <%s>" % from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = Header(sub, "utf-8")
+
+    server = smtplib.SMTP(smtp_server, 25)
+    # server.set_debuglevel(1)
+    server.login(from_addr, password)
+    server.sendmail(from_addr, [to_addr], msg.as_string())
+    server.quit()
