89a06fe5e54f2360b06f483366d61112b8a2fbd2
yueyongyue <yongpeng1@staff.sina.com.cn>
2018-06-19 08:21:42 +0800

    Changed event exclude mine.update

--- tasks/sse_worker.py
+++ tasks/sse_worker.py
@@ -12,16 +12,19 @@ def see_worker():
     db.close_mysql()
     if status is True and result:
         for product in result:
-            #job_pattern = re.compile('salt/job/\d+/ret/')
+            # job_pattern = re.compile('salt/job/\d+/ret/')
+            mine_pattern = re.compile(r'"fun": "mine.update"')
             salt_api = salt_api_for_product(product["id"])
             event_response = salt_api.events()
             client = sseclient.SSEClient(event_response)
             for event in client.events():
                 print(event.data)
-                # if job_pattern.search(event.data):
-                event_dict = ast.literal_eval(event.data.replace('true', 'True').replace('false', 'False').
-                                              replace('null', '""'))
-                event_dict['data']['product_id'] = product["id"]
-                db = DB()
-                db.insert("event", json.dumps(event_dict, ensure_ascii=False))
-                db.close_mysql()
+                if mine_pattern.search(event.data):
+                    pass
+                else:
+                    event_dict = ast.literal_eval(event.data.replace('true', 'True').replace('false', 'False').
+                                                  replace('null', '""'))
+                    event_dict['data']['product_id'] = product["id"]
+                    db = DB()
+                    db.insert("event", json.dumps(event_dict, ensure_ascii=False))
+                    db.close_mysql()
