96717b7394e334449841789e590ab2fd002010db
yueyongyue <yongpeng1@staff.sina.com.cn>
2018-06-22 09:15:06 +0800

    Changed add host where product_id

--- system/host.py
+++ system/host.py
@@ -158,7 +158,8 @@ class Hosts(object):
     def add_host(minion_list, product_id, user):
         db = DB()
         for minion_id in minion_list:
-            select_status, select_result = db.select("host", "where data -> '$.minion_id'='%s'" % minion_id)
+            select_status, select_result = db.select("host", "where data -> '$.minion_id'='%s' "
+                                                             "and data -> '$.product_id='%s'" % (minion_id, product_id))
             if select_status is False:
                 logger.error("Add % host error: %s" % (minion_id, select_result))
                 continue
