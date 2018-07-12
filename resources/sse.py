9c2e2e71705ff10c98044e88035b9a9680229c8e
yueyongyue <yongpeng1@staff.sina.com.cn>
2018-07-12 17:59:33 +0800

    Added sse error

--- resources/sse.py
+++ resources/sse.py
@@ -1,7 +1,11 @@
 # -*- coding:utf-8 -*-
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from common.db import DB
+from flask import g
 from tasks.tasks import event_to_mysql
+
+parser = reqparse.RequestParser()
+parser.add_argument("action", type=str, required=True, trim=True)
 
 
 class SSE(Resource):
@@ -9,7 +13,12 @@ class SSE(Resource):
         db = DB()
         status, result = db.select("product", "")
         db.close_mysql()
-        if status is True and result:
-            for product in result:
-                event_to_mysql.delay(product['id'])
-        return {"data": "", "status": True, "message": ""}
+        if status is True:
+            if result:
+                for product in result:
+                    event_to_mysql.delay(product['id'])
+                return {"data": "", "status": True, "message": ""}, 200
+            else:
+                return {"data": "", "status": False, "message": "Product does not exist"}, 404
+        else:
+            return {"data": "", "status": False, "message": result}, 500
