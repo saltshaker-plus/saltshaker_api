b9000878f24c11f252fd7369d61dee9b14792d67
yueyongyue <yongpeng1@staff.sina.com.cn>
2018-08-11 11:42:30 +0800

    删除：删除无用代码

--- scheduler/period_scheduler.py
+++ scheduler/period_scheduler.py
@@ -39,12 +39,8 @@ def scheduler_timing_modify(period_id, product_id, user, run_date):
 def scheduler_interval_add(period_id, product_id, user, run_interval, interval):
     if interval == "second":
         try:
-            print(run_interval)
-            print(period_id)
-            print(scheduler)
             scheduler.add_job(func="tasks.tasks:job", trigger='interval', seconds=run_interval,
                               args=[period_id, product_id, user], id=period_id)
-            print("ddddddddd")
             return {"status": True, "message": ""}
         except Exception as e:
             logger.error("Add second period scheduler error: %s" % e)
@@ -179,26 +175,3 @@ def scheduler_resume(period_id):
     except Exception as e:
         logger.error("Resume period scheduler error: %s" % e)
         return {"status": False, "message": str(e)}
-
-
-def my_job1():
-    times = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-    with open(r'/tmp/rs.txt', 'a') as f:
-        f.write(str(times) + '\n')
-    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
-
-
-def test():
-    scheduler.add_job(func="app:my_job1", id="ddddkkkkkk", trigger='interval', seconds=5)
-
-
-def test1():
-    scheduler.delete_job(id="erere")
-    #scheduler.add_job(func="scheduler.period_scheduler:my_job", id="dfdfdf", trigger='interval', seconds=5)
-
-def test2():
-    scheduler.add_job(func="scheduler.period_scheduler:my_job", id="time", trigger='date', run_date="2018-07-04 14:01:00")
-
-if __name__=="__main__":
-    from common.utility import local_to_utc
-    print(local_to_utc("2018-07-04 00:00:00"))
