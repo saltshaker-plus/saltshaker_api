84ef164c02accf96160143dbdb1a5b0bcb1ac993
Echo <yueyongyue@sina.cn>
2019-07-17 21:07:20 +0800

    Update send_mail.py

--- common/send_mail.py
+++ common/send_mail.py
@@ -21,7 +21,8 @@ def send_mail(to_addr, sub, content):
     msg['From'] = "Saltshaker <%s>" % from_addr
     msg['To'] = to_addr
     msg['Subject'] = Header(sub, "utf-8")
-
+    if not smtp_port：
+        smtp_port = 25
     server = smtplib.SMTP(smtp_server, smtp_port)
     # server.set_debuglevel(1)
     server.login(from_addr, password)
